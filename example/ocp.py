b4e000b14157e1260b034c8c5715ab5792f6d13c
akhdrzki <akhadirizky@gmail.com>
2024-06-06 13:05:33 +0800

    refactor: change status into str

--- example/ocp.py
+++ example/ocp.py
@@ -1,5 +1,8 @@
 """
 Here's a Open Closed, it means always open for extension but closed for modified existing class.
+We have a problem here. We try to add more payment class we can assume it as Paypal payment.
+The matter is Paypal doesn't use security code, its use email instead.
+So here Open Closed Principle solved the problem by define subclass of parent class (PaymentProcessor) 
 """
 
 from abc import ABC, abstractmethod
@@ -9,7 +12,7 @@ class Order:
     items = []
     quantities = []
     prices = []
-    status = []
+    status = "Open"
 
     def add_item(self, name: str, quantity: int, price: int) -> None:
         self.items.append(name)
@@ -27,17 +30,25 @@ class Order:
 """
 Abstract class that has similar purpose as Interface
 """
+
+
 class PaymentProcessor(ABC):
     @abstractmethod
     def pay(self, order: Order, security_code: str) -> None:
         pass
 
 
+"""
+Before we just define PaymentProcessor and write some method in it, here we created PaymentProcessor as Abstract Method, and created 
+few subclass that has its own responsibilites. The focused is don't modified whole exists code as new purposes.
+"""
+
+
 class DebitPaymentProcessor(PaymentProcessor):
     def pay(self, order: Order, security_code: str):
         print("processing debit payment")
         print(f"verifying security code {security_code}")
-        status = order.status == "paid"
+        status = order.status = "paid"
         print(status)
 
 
@@ -45,21 +56,20 @@ class CreditPaymentProcessor(PaymentProcessor):
     def pay(self, order: Order, security_code: str):
         print("processing credit payment")
         print(f"verifying security code {security_code}")
-        status = order.status == "paid"
-        print(status)
+        order.status = "paid"
 
 
 class GopayPaymentProcessor(PaymentProcessor):
     def pay(self, order: Order, security_code: str):
-        print("processing credit payment")
+        print("processing gopay payment")
         print(f"verifying security code {security_code}")
-        status = order.status == "paid"
+        status = order.status = "paid"
         print(status)
 
 
 def main():
     order = Order()
-    payment = GopayPaymentProcessor()
+    payment = DebitPaymentProcessor()
 
     order.add_item("sari roti", 3, 7500)
     order.add_item("indomilk choco", 2, 5300)
